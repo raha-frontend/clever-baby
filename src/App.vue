9dba86109226b4908a670e899dc18e4151d4a524
Rahman <52715445+raha-frontend@users.noreply.github.com>
2024-01-05 20:25:38 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,30 @@
 
 <style>
 	body {
+		margin: 0 auto;
 		font-family: 'Roboto', sans-serif;
+		width: 1366px;
+	}
+
+	:root {
+		--link-color: #122659;
+		--color-red: #ed1c21;
+	}
+
+	a {
+		display: block;
+		text-decoration: none;
+		color: var(--link-color);
+	}
+
+	ul {
+		padding: 0;
+		list-style: none;
+	}
+
+	.container {
+		margin-right: auto;
+		margin-left: auto;
+		padding: 0 98px;
 	}
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,87 @@
-<script setup lang="ts">
-	
+<script lang="ts">
+	import { defineComponent } from 'vue';
+
+	export default defineComponent({
+		name: 'TheHeader'
+	})
 </script>
 
 <template>
-	<div class="header">Потные яйца</div>
+	<header class="the-header">
+		<div class="the-header__inner container">
+			<div class="the-header__left">
+				<a class="the-header__link-logo" href="#">CleverBaby</a>
+
+				<ul class="the-header__menu">
+					<li class="the-header__item"><a href="#" class="the-header__menu-link">Методика</a></li>
+					<li class="the-header__item"><a href="#" class="the-header__menu-link">Преподаватели</a></li>
+					<li class="the-header__item"><a href="#" class="the-header__menu-link">Группы</a></li>
+					<li class="the-header__item"><a href="#" class="the-header__menu-link">Отзывы</a></li>
+					<li class="the-header__item"><a href="#" class="the-header__menu-link">Контакты</a></li>
+				</ul>
+			</div>
+
+			<div class="the-header__right">
+				<a href="tel:+74959834719" class="the-header__tel">+ 7 (495) 983-47-19</a>
+			</div>
+		</div>
+	</header>
 </template>
 
 <style scoped lang="scss">
+	.the-header {
+		padding: 14px 0;
 
+		&__inner {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+		}
+
+		&__left {
+			display: flex;
+			align-items: center;
+		}
+
+		&__link-logo {
+			margin-right: 47px;
+			font-size: 23px;
+			font-weight: 500;
+		}
+
+		&__menu {
+			display: flex;
+			align-items: center;
+		}
+
+		&__menu-link, 
+		&__tel {
+			padding: 10px;
+			font-size: 16px;
+			font-weight: 400;
+			line-height: 140%;
+
+			&:hover {
+				color: var(--color-red);
+				text-decoration: underline;
+      	transition: all 0.3s linear;
+			}
+		}
+
+		&__tel {
+			position: relative;
+			font-size: 18px;
+
+			&::before {
+				position: absolute;
+				content: "";
+				width: 15px;
+				height: 15px;
+				top: 15px;
+				left: -15px;
+				background-image: url(../assets/icons/phone.svg);
+				background-repeat: no-repeat;
+			}
+		}
+	}
 </style>
